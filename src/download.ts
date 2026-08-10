24ddb8d7c0a20ddd10c444db347349d89da9ac10
friuli-jokyo <26354730+friuli-jokyo@users.noreply.github.com>
2026-08-10 05:23:12 +0000

    fix: error handling

--- src/download.ts
+++ src/download.ts
@@ -13,13 +13,7 @@ const releaseUrl = latestRelease.data.assets[0].browser_download_url;
 
 console.log(`Latest release tag: ${releaseTagName}`);
 
-fs.writeFile("./bdf/version.txt", releaseTagName, (err) => {
-    if (err) {
-        console.error("Failed to save font version:", err);
-    } else {
-        console.log("Saved font version successfully.");
-    }
-});
+fs.writeFileSync("./bdf/version.txt", releaseTagName);
 
 const { entries } = await unzip(releaseUrl);
 const fileCheckList = new Map(FILE_NAMES.map(name => [name, false]));
@@ -28,17 +22,12 @@ for (const [name, entry] of Object.entries(entries)) {
     if (!fileCheckList.has(name)) continue;
     fileCheckList.set(name, true);
     const dstPath = `./bdf/${name}`;
-    fs.writeFile(dstPath, new Uint8Array(await entry.arrayBuffer()), (err) => {
-        if (err) {
-            console.error(`Failed to save ${dstPath}:`, err);
-        } else {
-            console.log(`Saved ${dstPath} successfully.`);
-        }
-    });
+    fs.writeFileSync(dstPath, new Uint8Array(await entry.arrayBuffer()));
 }
 
 for (const [name, found] of fileCheckList.entries()) {
     if (!found) {
         console.error(`Missing expected file: ${name}`);
+        process.exit(1);
     }
 }
